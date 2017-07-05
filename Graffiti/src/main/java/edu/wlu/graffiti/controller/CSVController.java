5722ce5907f55793a849711b0407716cf221b845
Hammad Ahmad <ahmadh19@mail.wlu.edu>
2017-07-05 12:36:16 -0400

    fixed downloaded filenames for CSV exports

--- Graffiti/src/main/java/edu/wlu/graffiti/controller/CSVController.java
+++ Graffiti/src/main/java/edu/wlu/graffiti/controller/CSVController.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.wlu.graffiti.bean.Inscription;
@@ -28,20 +30,23 @@ public class CSVController {
 	private FindspotDao findspotDao;
 
 	@RequestMapping(value = "/graffito/AGP-{edrId}/csv", produces = "text/csv")
-	public String getInscription(@PathVariable String edrId) {
+	public String getInscription(@PathVariable String edrId, HttpServletResponse response) {
+		response.addHeader("Content-Disposition", "attachment; filename=AGP-"+ edrId +".csv");
 		return generator.serializeToCSV(graffitiDao.getInscriptionByEDR(edrId));
 	}
 	
 	@RequestMapping(value = "/all/csv", produces = "text/csv")
-	public String getInscriptions() {
+	public String getInscriptions(HttpServletResponse response) {
+		response.addHeader("Content-Disposition", "attachment; filename=all-inscriptions.csv");
 		return generator.serializeToCSV(graffitiDao.getAllInscriptions());
 	}
 	
 	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/filtered-results/csv", produces = "text/csv")
-	public String getFilteredInscriptions(final HttpServletRequest request) {
+	public String getFilteredInscriptions(final HttpServletRequest request, HttpServletResponse response) {
 		HttpSession s = request.getSession();
 		List<Inscription> results = (List<Inscription>) s.getAttribute("filteredList");
+		response.addHeader("Content-Disposition", "attachment; filename=filtered-results.csv");
 		return generator.serializeToCSV(results);
 	}
 	
